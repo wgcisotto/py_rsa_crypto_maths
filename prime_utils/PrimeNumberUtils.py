85c549f03786c85e82d649f48b4618fee169a145
William Cisotto <willgalindo@gmail.com>
2023-10-28 19:43:12 +0100

    update to add documentation

--- prime_utils/PrimeNumberUtils.py
+++ prime_utils/PrimeNumberUtils.py
@@ -1,33 +1,68 @@
-from decimal import Decimal
 import random
-
-from decimal import Decimal, getcontext
+from decimal import Decimal
 
 
 # Prime Numbers Checker Util Class
 class PrimeNumberUtils:
 
+    """
+    A utility class for checking if a number is prime.
+    """
+
     @staticmethod
     def is_prime_by_trial_error(n):
+        """
+        Checks if a number is prime using the trial division method.
+
+        Args:
+            n: The number to check.
+
+        Returns:
+            True if the number is prime, False otherwise.
+        """
+
         if n <= 1:
             return False
         if n <= 3:
             return True
         if n % 2 == 0 or n % 3 == 0:
             return False
+
         i = 5
         while i * i <= n:
             if n % i == 0:
                 return False
             i += 2
+
         return True
 
     @staticmethod
     def is_prime_by_miller_rabin(n):
+        """
+        Checks if a number is prime using the Miller-Rabin primality test.
+
+        Args:
+            n: The number to check.
+
+        Returns:
+            True if the number is prime, False otherwise.
+        """
+
         return PrimeNumberUtils.is_prime(n)
 
     @staticmethod
     def is_prime(n, k=10):
+        """
+        Checks if a number is prime using the Miller-Rabin primality test with a given number of iterations.
+
+        Args:
+            n: The number to check.
+            k: The number of iterations to use in the Miller-Rabin test.
+
+        Returns:
+            True if the number is prime, False otherwise.
+        """
+
         if n <= 1 or n == 4:
             return False
         if n <= 3:
@@ -40,10 +75,22 @@ class PrimeNumberUtils:
         for i in range(k):
             if not PrimeNumberUtils.miller_test(d, n):
                 return False
+
         return True
 
     @staticmethod
     def miller_test(d, n):
+        """
+        Performs a single iteration of the Miller-Rabin primality test.
+
+        Args:
+            d: The odd part of the number to be tested.
+            n: The number to be tested.
+
+        Returns:
+            True if the number passes the Miller-Rabin test, False otherwise.
+        """
+
         a = Decimal(random.randint(2, int(n) - 2))
         x = PrimeNumberUtils.power(a, d, n)
 
@@ -64,6 +111,18 @@ class PrimeNumberUtils:
 
     @staticmethod
     def power(x, y, p):
+        """
+        Computes the modular exponentiation of a number.
+
+        Args:
+            x: The base.
+            y: The exponent.
+            p: The modulus.
+
+        Returns:
+            The result of the modular exponentiation.
+        """
+
         res = 1
         x = x % p
         while y > 0:
@@ -72,3 +131,22 @@ class PrimeNumberUtils:
             y = y // 2
             x = (x * x) % p
         return res
+
+    @staticmethod
+    def generate_random_prime_number():
+        """
+        Generates a random prime number.
+
+        Returns:
+            A random prime number.
+        """
+
+        # Generate a random number.
+        random_number = random.randint(2, 1000)
+
+        # Check if the number is prime.
+        if PrimeNumberUtils.is_prime(random_number):
+            return random_number
+        else:
+            # If the number is not prime, generate a new one.
+            return PrimeNumberUtils.generate_random_prime_number()
--- rsa/rsa.py
+++ rsa/rsa.py
@@ -4,13 +4,23 @@ import math
 from prime_utils.PrimeNumberUtils import PrimeNumberUtils
 
 
-def generate_private_key(p1, p2):
-    factors = factorize(p1)
+def generate_private_key(n, e):
+    """
+    Generate a private key from given values.
+
+    Args:
+        n (int): Modulus for the RSA key pair.
+        e (int): Public exponent (usually referred to as 'e').
+
+    Returns:
+        tuple: A tuple containing the public key (e, n) and private key (d, n).
+    """
+    factors = factorize(n)
     q, p = factors[0], factors[1]
     n = q * p
 
-    e = p2
-    inverse = mod_inverse(e, (q - 1) * (p - 1))
+    d = e  # The public exponent is typically used as the private exponent.
+    inverse = mod_inverse(d, (q - 1) * (p - 1))
 
     public_key = (e, n)
     private_key = (inverse, n)
@@ -19,6 +29,12 @@ def generate_private_key(p1, p2):
 
 
 def generate_keypair():
+    """
+    Generate a key pair with random prime numbers.
+
+    Returns:
+        tuple: A tuple containing the public key (e, n) and private key (d, n).
+    """
     q = PrimeNumberUtils.generate_random_prime_number()
     p = PrimeNumberUtils.generate_random_prime_number()
     n = q * p
@@ -34,6 +50,16 @@ def generate_keypair():
 
 
 def encrypt(publicKey, message):
+    """
+    Encrypt a message using a public key.
+
+    Args:
+        publicKey (tuple): A tuple containing the public key (e, n).
+        message (str): The message to be encrypted.
+
+    Returns:
+        list: A list of encrypted characters.
+    """
     # Unpack the key
     e, n = publicKey
     # Convert each letter in the plaintext to numbers based on the character using a^b mod m
@@ -42,15 +68,34 @@ def encrypt(publicKey, message):
 
 
 def decrypt(privateKey, message):
+    """
+    Decrypt a message using a private key.
+
+    Args:
+        privateKey (tuple): A tuple containing the private key (d, n).
+        message (list): A list of encrypted characters.
+
+    Returns:
+        str: The decrypted message.
+    """
     # Unpack the key
     d, n = privateKey
     # Generate the plaintext based on the ciphertext and key using a^b mod m
     p = [chr((char ** d) % n) for char in message]
-    # Return the array
+    # Return the array as a string
     return ''.join(p)
 
 
 def factorize(num):
+    """
+    Factorize a number into its prime factors.
+
+    Args:
+        num (int): The number to factorize.
+
+    Returns:
+        list: A list of prime factors.
+    """
     factors = []
     for i in range(2, int(math.sqrt(num)) + 1):
         while num % i == 0:
@@ -61,23 +106,39 @@ def factorize(num):
     return factors
 
 
-# TODO: This function could be improved by using a more efficient algorithm for finding a relatively prime number to
-#  the given input.
 def random_select_relatively_prime(phi):
+    """
+    Select a random number that is relatively prime to the given input.
+
+    Args:
+        phi (int): The input value.
+
+    Returns:
+        int: A randomly selected number that is relatively prime to phi.
+    """
     while True:
         e = random.randrange(2, phi)
-        if math.gcd(e, phi) == 1:  # TODO: change here to use the function implemented before.
+        if math.gcd(e, phi) == 1:
             return e
 
 
-# TODO: this function could be improved by using a more efficient algorithm for finding the modular inverse of a number.
 def mod_inverse(a, m):
+    """
+    Calculate the modular inverse of a number.
+
+    Args:
+        a (int): The number for which the modular inverse is calculated.
+        m (int): The modulus.
+
+    Returns:
+        int: The modular inverse of a with respect to m.
+    """
     m0 = m
     y = 0
     x = 1
-    if (m == 1):
+    if m == 1:
         return 0
-    while (a > 1):
+    while a > 1:
         # q is quotient
         q = a // m
         t = m
@@ -88,7 +149,7 @@ def mod_inverse(a, m):
         y = x - q * y
         x = t
         # Make x positive
-    if (x < 0):
+    if x < 0:
         x = x + m0
 
     return x
